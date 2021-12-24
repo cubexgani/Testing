eb65e124764a7558626b86d063d71b03f3eed7bf
cubexgani <mroybros30@gmail.com>
2021-12-24 22:08:41 +0530

    Write hello world but using functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+//normal console statement
 console.log("Hello world");
+
+//lambda
+let hello = () => console.log("Hello world");
+hello();
+console.log(hello);
+
+//function expression
+let hellof = function (){
+	console.log("Hello world");
+}
+console.log(hellof);
+hellof();
+
+//normal function
+function fhello(){
+	console.log("Hello world");
+}
+fhello();
+
