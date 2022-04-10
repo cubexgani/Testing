faab487241b3d3d4cc4dcdb7b2f6d173d5bcefda
cubexgani <mroybros30@gmail.com>
2022-04-10 15:32:25 +0530

    Pog numbers

---
+++ datTypes.js
@@ -0,0 +1,16 @@
+my_dog = 16;	//yep my dog's officially a highschooler now, going after bitches
+random_octal_digit = 0o4521065;		//L
+random_binary_digit = 0b0011000101001;	//ok I'll stop using underscores y'all
+randomHexDigit = 0xDED;		//me when
+randomExpDigit = 69.42e6;
+
+console.log(`${my_dog} ${random_octal_digit} ${random_binary_digit} ${randomHexDigit} ${randomExpDigit}`);
+
+console.log(625000000..toExponential());
+console.log(65.1985236.toFixed(3));
+console.log(Math.round(4.256));
+console.log(parseInt("123px"));
+console.log(Math.round(Math.random() * 100));	//definitely won't get a 69!... or will I
+console.log(`Maximum and minimum of 6 random numbers you don't know are ${Math.max(1,8,3,-2,10,-7)} ${Math.min(1,8,3,-2,10,-7)}`);
+console.log(isFinite(Infinity));	//me when the
+console.log(isNaN("Kobusg"));
