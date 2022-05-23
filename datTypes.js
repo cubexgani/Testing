51a31f25b630a16ff5143b5c4ac8d7bf578f3c92
cubexgani <mroybros30@gmail.com>
2022-05-23 19:46:47 +0530

    Strongs

--- datTypes.js
+++ datTypes.js
@@ -1,8 +1,8 @@
-my_dog = 16;	//yep my dog's officially a highschooler now, going after bitches
-random_octal_digit = 0o4521065;		//L
-random_binary_digit = 0b0011000101001;	//ok I'll stop using underscores y'all
-randomHexDigit = 0xDED;		//me when
-randomExpDigit = 69.42e6;
+let my_dog = 16;	//my 16 year old dog got no bitches
+let random_octal_digit = 0o452_10_65;		//L
+let random_binary_digit = 0b0011000101001;	//ok I'll stop using underscores y'all
+let randomHexDigit = 0xDED;		//me when
+let randomExpDigit = 69.42e6;
 
 console.log(`${my_dog} ${random_octal_digit} ${random_binary_digit} ${randomHexDigit} ${randomExpDigit}`);
 
@@ -14,3 +14,15 @@ console.log(Math.round(Math.random() * 100));	//definitely won't get a 69!... or
 console.log(`Maximum and minimum of 6 random numbers you don't know are ${Math.max(1,8,3,-2,10,-7)} ${Math.min(1,8,3,-2,10,-7)}`);
 console.log(isFinite(Infinity));	//me when the
 console.log(isNaN("Kobusg"));
+
+let str = `Joe got no maidens. One day he went to the grocery store to buy some milk and met gigachad. That incident was the turning point in his life.`
+console.log(str);
+console.log(`Joe was ${str.substring(str.indexOf('no') + 3, str.indexOf('.') - 1) + 'less'}.`);
+console.log(
+	`Gonna give you up
+	Gonna let you down
+	Gonna run around and desert you.`.includes('Never')
+);
+console.log('E'.codePointAt(0));
+console.log(String.fromCodePoint(76));
+
